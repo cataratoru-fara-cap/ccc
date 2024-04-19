0795a5ca957341a89e2b6d91a3448f3394e216cb
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-04-19 20:13:33 +0300

    last progress at th e end of the contest

--- lvl3/gabi.py
+++ lvl3/gabi.py
@@ -7,21 +7,73 @@ input = open(my_file, "r")
 
 nr_splits = int(input.readline())# noqa number of splits
 lawns = []
-lawn = []
 
-size = input.readline()
+size = input.readline().strip()
 lh = int(size.split()[1])
 path = ""
-while (input):
+while (True):
+    lawn = []
     for i in range(lh):
         lawn.append(input.readline().strip())
-    lawns.append(lawn)
+
     path = input.readline().strip()
+    lawn.append(path)
+    lawns.append(lawn)
 
     size = input.readline()
-    lh = int(size.split()[1])
+    if (size == ""):
+        break
+    lh = int(size.strip().split()[1])
 
-print(lawns)
+
+# PARSING ENDS HERE
+def check_path(matrix, path, start, L, H):
+    valid = "NOT VALID"
+    free_parcels = 0
+    for i in range(L):
+        for j in range(H):
+            if (matrix[i][j] != "X"):
+                free_parcels += 1
+
+    if (len(path) != free_parcels):
+        return "INVALID"
+
+    for i in range(L):
+        for j in range(H):
+            V = matrix
+            if (start == "X"):
+                break
+
+            x = start[0]
+            y = start[1]
+
+            for c in path:
+                if c == "A":
+                    x -= 1
+                elif c == "D":
+                    x += 1
+                elif c == "W":
+                    y += 1
+                elif c == "S":
+                    y -= 1
+
+                if x > (L-1) or y > (H-1) or (x < 0) or (y < 0):
+                    break
+                elif (V[x][y] == "X"):
+                    break
+                else:
+                    V[x][y] = "X"
+            valid = "VALID"
+            return valid
+
+
+
+
+
+
+
+
+
 """ output_file = sys.argv[2]
 output = open(output_file, "w")
 
