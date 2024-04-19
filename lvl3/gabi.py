9c2479f25166ba6bfa0cd70e66c89e79ec1e17e6
cataratoru-fara-cap <gabivaireanu@gmail.com>
2024-04-19 18:00:00 +0300

    translated code

--- lvl3/gabi.py
+++ lvl3/gabi.py
@@ -10,36 +10,15 @@ output = open(output_file, "w")
 for line in lines:
 
     for c in line:
-        if c == "A" and x_cur == x_min:
-            x_cur -= 1
-            x_min -= 1
-
-        elif c == "W" and y_cur == y_max:
-            y_cur += 1
-            y_max += 1
-
-        elif c == "S" and y_cur == y_min:
-            y_cur -= 1
-            y_min -= 1
-
-        elif c == "D" and x_cur == x_max:
-            x_cur += 1
-            x_max += 1
-
-        elif c == "A":
-            x_cur -= 1
+        if c == "A":
 
         elif c == "W":
-            y_cur += 1
+
 
         elif c == "S":
-            y_cur -= 1
+
 
         elif c == "D":
-            x_cur += 1 
-
-    H = y_max - y_min + 1
-    L = x_max - x_min + 1
 
     output.write(f"{L} {H}\n")
 
